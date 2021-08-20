701a88759fd1ee9b7f89eab1860bb5eb11397d63
jks5177 <53725808+jks5177@users.noreply.github.com>
2021-08-20 14:28:09 +0900

    add_comment

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import flask
 from flask import Flask, render_template, Response
 from flask import Flask, url_for, render_template, request, redirect, session
-#from models import db
-#from models import Fcuser
-#from flask_wtf.csrf import CSRFProtect
-#from forms import ResiterForm,LoginForm
+# from model import db
+# from model import Fcuser
+# from flask_wtf.csrf import CSRFProtect
+# from form import ResiterForm,LoginForm
 import os
 from importlib import import_module
 import cv2
@@ -68,48 +68,49 @@ def preprocess(img) :
 cam = cv2.VideoCapture(cv2.CAP_DSHOW+1)
 # cam = cv2.VideoCapture(cv2.CAP_DSHOW)
 
-#사용자 등록 페이지
-@app.route('/register', methods=['GET','POST'])
-def register():
-	if request.method =='GET':
-		return render_template("register.html")
-	else:
-		user_name = request.form.get('user_name')
-		user_company = request.form.get('user_company')
-
-		if not (user_name and user_company):
-			return "모두 입력해주세요"
-		else:
-			user = User()
-			user.user_name = user_name
-			user.user_company = user_company
-			db.session.add(user)
-			db.session.commit()
-			return "회원가입 완료"
-		return redirect('/')
-#로그인 페이지
-# login 페이지 접속(GET) 처리와, "action=/login" 처리(POST)처리 모두 정의
-@app.route('/login', methods=['GET', 'POST'])	
-def login():
-	if request.method=='GET':
-		return render_template('login.html')
-	else:
-		user_company = request.form['user_company']
-		user_name = request.form['user_name']
-		try:
-			data = User.query.filter_by(user_company=user_company, user_name=user_name).first()	# ID/PW 조회Query 실행
-			if data is not None:	# 쿼리 데이터가 존재하면
-				session['user_company'] = user_company	# userid를 session에 저장한다.
-				return redirect('/')
-			else:
-				return 'Dont Login'	# 쿼리 데이터가 없으면 출력
-		except:
-			return "dont login"	# 예외 상황 발생 시 출력
-
-@app.route('/logout', methods=['GET'])
-def logout():
-	session.pop('userid', None)
-	return redirect('/') 
+# #사용자 등록 페이지
+# @app.route('/register', methods=['GET','POST'])
+# def register():
+# 	if request.method =='GET':
+# 		return render_template("register.html")
+# 	else:
+# 		user_name = request.form.get('user_name')
+# 		user_company = request.form.get('user_company')
+#
+# 		if not (user_name and user_company):
+# 			return "모두 입력해주세요"
+# 		else:
+# 			user = User()
+# 			user.user_name = user_name
+# 			user.user_company = user_company
+# 			db.session.add(user)
+# 			db.session.commit()
+# 			return "회원가입 완료"
+# 		return redirect('/')
+#
+# #로그인 페이지
+# # login 페이지 접속(GET) 처리와, "action=/login" 처리(POST)처리 모두 정의
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+# 	if request.method=='GET':
+# 		return render_template('login.html')
+# 	else:
+# 		user_company = request.form['user_company']
+# 		user_name = request.form['user_name']
+# 		try:
+# 			data = User.query.filter_by(user_company=user_company, user_name=user_name).first()	# ID/PW 조회Query 실행
+# 			if data is not None:	# 쿼리 데이터가 존재하면
+# 				session['user_company'] = user_company	# userid를 session에 저장한다.
+# 				return redirect('/')
+# 			else:
+# 				return 'Dont Login'	# 쿼리 데이터가 없으면 출력
+# 		except:
+# 			return "dont login"	# 예외 상황 발생 시 출력
+#
+# @app.route('/logout', methods=['GET'])
+# def logout():
+# 	session.pop('userid', None)
+# 	return redirect('/')
     
 
 #메인 페이지
@@ -123,14 +124,14 @@ def gen_frames():  # generate frame by frame from camera
         # Capture frame-by-frame
         ret, frame = cam.read()  # read the camera frame
         
-        if (not ret):
+        if (not ret): # 프레임이 없을 경우
             break
-        else:
-            frame_ = preprocess(frame)
-            ret, buffer = cv2.imencode('.jpg', frame_)
-            frame = buffer.tobytes()
+        else: # 프레임이 있을 경우
+            frame_ = preprocess(frame) # 전처리 진행(가이드 라인 생성)
+            ret, buffer = cv2.imencode('.jpg', frame_) # frame을 jpg 파일로 인코딩 진행
+            frame = buffer.tobytes() # 데이터 전송을 위해 바이트 형으로 변환
             yield (b'--frame`\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') # html 해당 위치로 frmae 전송
                    # concat frame one by one and show result
             
 @app.route('/camera')
@@ -142,24 +143,28 @@ def camera_result():
     return render_template('camera_result.html')
 
 @app.route('/picture')
-def taking_picture():
+def taking_picture(): # 사진을 저장하는 페이지
     ret, frame = cam.read()  # read the camera frame
-    d = pytesseract.image_to_data(frame, output_type=Output.DICT)
-    print(d['text'])
+    d = pytesseract.image_to_data(frame, output_type=Output.DICT) # pytesseract로 ORC 검사
+    # print(d['text'])
     for i in range(len(d['text'])):
         # print(i)
         if (d['text'][i].startswith('KM') or d['text'][i].startswith('KN')) and len(d['text'][i]) > 15:
+            # text가 KM 또는 KN으로 시작하고 text의 길이가 15 이상인 것만 추출
             (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-            frame = cv2.rectangle(frame, (x-5, y-3), (x + w+10, y + h+6), (0, 255, 0), 2)
+            frame = cv2.rectangle(frame, (x-5, y-3), (x + w+10, y + h+6), (0, 255, 0), 2) # 해당 위치에 bounding box 생성
             text = d['text'][i]
 
+            # 파일 이름 생성
             now = time.localtime()
             s = '%04d-%02d-%02d-%02d-%02d-%02d' % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
             file_path = 'image/check_'+s+'.jpg'
 
+            # 파일 저장 시간.jpg는 매번 바뀌는 이미지, check.jpg는 저장할 수 있는 이미지
             cv2.imwrite('static/'+file_path,frame)
             cv2.imwrite('static/image/check.jpg', frame)
 
+            # text 파일 생성, 나중에 파일 이름으로 사용될 OCR 결과 값
             f = open("static/image/check.txt", 'w')
             f.write(d['text'][i])
             f.close()
@@ -172,7 +177,7 @@ def taking_picture():
     return flask.redirect(flask.url_for('camera'))
 
 @app.route('/del')
-def del_img():
+def del_img(): # 이미지 삭제
     import os
     path_dir = 'static/image'
     file_list = os.listdir(path_dir)
@@ -182,7 +187,7 @@ def del_img():
     return flask.redirect(flask.url_for('camera'))
 
 @app.route('/save', methods=['GET', 'POST'])
-def save_img():
+def save_img(): # 이미지 저장
     import os
     img = cv2.imread('static/image/check.jpg')
 
@@ -194,6 +199,7 @@ def save_img():
     file_path = 'static/complete/' + str(text) + '.jpg'
     cv2.imwrite(file_path, img)
 
+    # temp 폴더 내 파일 제거
     path_dir = 'static/image'
     file_list = os.listdir(path_dir)
     for filename in file_list:
@@ -204,7 +210,7 @@ def save_img():
 
 
 @app.route('/video_feed')
-def video_feed():
+def video_feed(): # 프레임을 실시간으로 전송해주는 페이지
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
