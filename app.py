2617478d06cb5341d89dd0bc16b869490a2cc713
jks5177 <53725808+jks5177@users.noreply.github.com>
2021-08-25 10:50:26 +0900

    main_page_back_0823

--- app.py
+++ app.py
@@ -98,68 +98,9 @@ def preprocess(img) :
                 cv2.line(img, (i[0][0]+int(j*0.01*length), i[0][1]),(i[0][0]+int((j+1)*0.01*length), i[0][1]), (42,204,246), 2)
     return img
 
-
-#camera = cv2.VideoCapture(0)
-
 #0은 전면, 1은 후면
 
-<<<<<<< HEAD
-cam = cv2.VideoCapture(cv2.CAP_DSHOW+2)
-=======
-
-cam = cv2.VideoCapture(cv2.CAP_DSHOW+0)
->>>>>>> 6416fa5827ed72f8346ccb436b9416815ab75626
-# cam = cv2.VideoCapture(cv2.CAP_DSHOW)
-
-# #사용자 등록 페이지
-# @app.route('/register', methods=['GET','POST'])
-# def register():
-# 	if request.method =='GET':
-# 		return render_template("register.html")
-# 	else:
-# 		user_name = request.form.get('user_name')
-# 		user_company = request.form.get('user_company')
-#
-# 		if not (user_name and user_company):
-# 			return "모두 입력해주세요"
-# 		else:
-# 			user = User()
-# 			user.user_name = user_name
-# 			user.user_company = user_company
-# 			db.session.add(user)
-# 			db.session.commit()
-# 			return "회원가입 완료"
-# 		return redirect('/')
-#
-# #로그인 페이지
-# # login 페이지 접속(GET) 처리와, "action=/login" 처리(POST)처리 모두 정의
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-# 	if request.method=='GET':
-# 		return render_template('login.html')
-# 	else:
-# 		user_company = request.form['user_company']
-# 		user_name = request.form['user_name']
-# 		try:
-# 			data = User.query.filter_by(user_company=user_company, user_name=user_name).first()	# ID/PW 조회Query 실행
-# 			if data is not None:	# 쿼리 데이터가 존재하면
-# 				session['user_company'] = user_company	# userid를 session에 저장한다.
-# 				return redirect('/')
-# 			else:
-# 				return 'Dont Login'	# 쿼리 데이터가 없으면 출력
-# 		except:
-# 			return "dont login"	# 예외 상황 발생 시 출력
-#
-# @app.route('/logout', methods=['GET'])
-# def logout():
-# 	session.pop('userid', None)
-# 	return redirect('/')
-
-<<<<<<< HEAD
 cam = cv2.VideoCapture(cv2.CAP_DSHOW+1)
-=======
-# cam = cv2.VideoCapture(cv2.CAP_DSHOW+1)
->>>>>>> 6416fa5827ed72f8346ccb436b9416815ab75626
 
 #사용자 등록 페이지
 @app.route('/register', methods=['GET','POST'])
@@ -205,13 +146,7 @@ def login_page():
 @app.route('/logout', methods=['GET'])
 def logout():
 	session.pop('userid', None)
-<<<<<<< HEAD
 	return redirect('/') 
-
-    
-=======
-	return redirect('/')
->>>>>>> 6416fa5827ed72f8346ccb436b9416815ab75626
 
 #메인 페이지
 @app.route('/main')
@@ -361,5 +296,5 @@ def table():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run('localhost', 4997)
 
